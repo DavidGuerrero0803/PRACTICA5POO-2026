15f2cc3c59055f7ebcfecc4da0f8eafb2ecda413
David <mario.guerrero.marquez@uabc.edu.mx>
2026-05-12 22:44:39 -0700

    Creada la clase Consola. Esta clase se encargará de manejar la interacción con el usuario a través de una interfaz de usuario (UI). Pudiendo elegir dificultad e idioma.

---
+++ src/main/java/uabc/david/practica5poo2026/Consola.java
@@ -0,0 +1,42 @@
+package uabc.david.practica5poo2026;
+
+import java.util.Scanner;
+
+public class Consola {
+    private Betweenle juego;
+    private Scanner scanner;
+
+    public Consola() {
+        this.scanner = new Scanner(System.in);
+        this.juego = new Betweenle(3);
+    }
+
+    public void iniciar() {
+
+    }
+
+    public void mostrarMenu() {
+
+    }
+
+    public void ejecutarPartida() {
+
+    }
+
+    public void mostrarHistorial() {
+
+    }
+
+    public void mostrarLetrasUsadas() {
+
+    }
+
+    public void gestionarPista() {
+
+    }
+
+    public static void main(String[] args) {
+        Consola ui = new Consola();
+        ui.iniciar();
+    }
+}
