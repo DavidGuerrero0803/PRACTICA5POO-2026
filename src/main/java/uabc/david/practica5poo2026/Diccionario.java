9f4a4219fd6e0f9975d305448cfea4c7fdc00c73
David <mario.guerrero.marquez@uabc.edu.mx>
2026-05-12 23:09:15 -0700

    Agregados dos métodos en la clase Diccionario, uno para cargar tanto el archivo en español como en inglés, y otro método que permite agregar palabras al diccionario.

--- src/main/java/uabc/david/practica5poo2026/Diccionario.java
+++ src/main/java/uabc/david/practica5poo2026/Diccionario.java
@@ -1,5 +1,8 @@
 package uabc.david.practica5poo2026;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class Diccionario {
@@ -17,5 +20,25 @@ public class Diccionario {
 
     public int getPalabrasTotales() {
         return palabras.size();
+    }
+
+    public void cargarDesdeArchivo(String rutaArchivo) {
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(rutaArchivo));
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String palabra = linea.trim().toLowerCase();
+                if (!palabra.isEmpty()) {
+                    palabras.put(palabra, "");
+                }
+            }
+            lector.close();
+        } catch (IOException e) {
+            System.out.println("Se produjo un error al cargar el archivo " + rutaArchivo);
+        }
+    }
+
+    public void agregarPalabra(String palabra, String definicion) {
+        palabras.put(palabra.toLowerCase(), definicion);
     }
 }
